abbabac77fb3e706a310b3550f4c7c0f7abc0b50
akvilm <akvilm@gmail.com>
2022-09-23 11:00:59 -0700

    Changes part 2:)

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -27,5 +27,5 @@ public class MainActivity extends AppCompatActivity {
         tvMain.setTextColor(Color.BLUE);
     }
 
-    ///Some new changes that we do not need
+    ///Some changes that we do not need part 2 :)
 }
